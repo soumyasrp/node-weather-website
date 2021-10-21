a9e45c8c0d2ce581b8e195752443fb4e1ef89e25
SOUMYA RANJAN PATI <soumyasrp56@gmail.com>
2021-10-22 02:01:53 +0530

    Add Humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request=require("request")
 
+
+
 const forecast=(lat,long,callback)=>{
     const url="http://api.weatherstack.com/current?access_key=6bdc8835fa847bc6021526822bad5527&query=" + lat + "," + long +"&units=m"
     request({url,json:true},(error,{body})=>{
@@ -12,7 +14,8 @@ const forecast=(lat,long,callback)=>{
 
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out."+" It feels like "+body.current.feelslike+" degrees out.");
+          
+            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out."+" It feels like "+body.current.feelslike+" degrees out. Its Humidity is about "+body.current.humidity+"% .");
 
 
         }
